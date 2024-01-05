9ad24914ba2c5d580076a60f1cfc47753ccc4e94
djukipuki <Angel309933gmail.com>
2024-01-06 01:43:06 +0200

    hw

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -2,9 +2,10 @@
   Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
+type DataType = [string, number];
 
-function getPromise () {
-  return new Promise((resolve) => {
+function getPromise (): Promise<DataType> {
+  return new Promise<DataType>((resolve) => {
     resolve(['Text', 50]);
   });
 }
